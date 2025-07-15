2c840937595356c4b7c1b7e607e3eca7cd9024de
~Master~ <62647000+Master-Rus@users.noreply.github.com>
2025-07-16 06:44:41 +0700

    Add

---
+++ lang/en.py
@@ -0,0 +1,19 @@
+# lang/en.py
+
+texts = {
+    'title': '🛠️ 3x-ui API Viewer',
+    'search_label': '🔍 Search:',
+    'connect_button': '🔄 Connect',
+    'export_button': '📤 Export CSV',
+    'language_label': 'Select language:',
+}
+
+columns_info = {
+    "Email": 180,
+    "Port": 60,
+    "Protocol": 80,
+    "End Date": 130,
+    "Enable": 80,
+    "Traffic Limit": 120,
+    "Traffic": 250
+}
